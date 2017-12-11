3dc4d7c4d29501062d795a689fde2bc2dd2e5e79
AndriyPytel <andriypytel@gmail.com>
2017-12-12 01:54:11 +0200

    create level 1

---
+++ Game_Restoration/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public Player Player;
+
+    private void Start()
+    {
+
+        Player = Player == null ? GetComponent<Player>() : Player;
+        if (Player == null)
+        {
+            Debug.LogError("Player not set to controller");
+        }
+    }
+
+    private void Update()
+    {
+
+        if (Player != null)
+        {
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                Player.MoveRight();
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                Player.MoveLeft();
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Player.Jump();
+            }
+        }
+    }
+}
