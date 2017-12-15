64d52e06eadfbfc3d712d50a41a3ed5dfe39016a
SerhiiShcheh <32397218+SerhiiShcheh@users.noreply.github.com>
2017-12-15 03:34:51 +0200

    Розширення карти

---
+++ Game_Restoration/Assets/Scripts/InventarLibrary.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventarLibrary : MonoBehaviour {
+
+	public string[] Names;
+	public Texture[] Images;
+}
